7c5abf199bc88c9586fefc9fcfaa8d699ddc6b00
Thiago <thiago.barbosa@nkey.digital>
2024-02-03 21:14:37 -0300

    fix: erro de autenticacao no password

--- src/main/resources/db/migration/V1_0_2__create_user.sql
+++ src/main/resources/db/migration/V1_0_2__create_user.sql
@@ -1,4 +1,4 @@
 insert into pnt_user (id, username, password, role)
-values(1, 'teste', '$2a$10$T1Au9GvLFm3BJ1WlSUkwbOFfc3Zxxh4uyG5gNTfsUuuBgT43341HS', 'USER');
+values(1, 'teste', '$2a$10$n.METGGR7mpULDA/3MIGXeEpEq4pRFsU7vizKqCXz9feDCHwXW7Ki', 'USER');
 insert into pnt_user (id, username, password, role)
 values(2, 'teste2', '$2a$10$k6k4KJGjqaTWZ6FSBkBYT.WzOFgI7rYf0kKMiRo7TM9cCCWPb4MOG', 'USER');
